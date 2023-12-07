abe19df5f871e775a8da252a803571d799b76545
Jared Corker <jared.corker@gmail.com>
2023-12-07 23:58:56 +1100

    update options.lua

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -2,3 +2,17 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.scrolloff = 8
