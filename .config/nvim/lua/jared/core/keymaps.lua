2a1ec8595ee3866a2169cba211da739e4b2cee9d
Jared Corker <jared.corker@gmail.com>
2023-12-13 00:23:41 +1100

    add window movement keymaps

--- .config/nvim/lua/jared/core/keymaps.lua
+++ .config/nvim/lua/jared/core/keymaps.lua
@@ -6,6 +6,10 @@ vim.g.mapleader = " "
 keymap("n", "<leader>pf", ":Ex<CR>")
 keymap("n", "<leader>qq", ":qa<CR>")
 keymap("n", "<leader>l", ":Lazy<CR>")
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
 
 -- Insert mode
 keymap("i", "kj", "<Esc>")
