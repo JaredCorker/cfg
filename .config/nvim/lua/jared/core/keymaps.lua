02f2231e83bdb901b418e5954a6dc40d1d2c7f0d
Jared Corker <jared.corker@gmail.com>
2023-12-13 00:24:37 +1100

    disable netrw

--- .config/nvim/lua/jared/core/keymaps.lua
+++ .config/nvim/lua/jared/core/keymaps.lua
@@ -3,7 +3,6 @@ local keymap = vim.keymap.set
 vim.g.mapleader = " "
 
 -- Normal mode
-keymap("n", "<leader>pf", ":Ex<CR>")
 keymap("n", "<leader>qq", ":qa<CR>")
 keymap("n", "<leader>l", ":Lazy<CR>")
 keymap("n", "<C-h>", "<C-w>h")
