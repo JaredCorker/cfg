f4af2af38a7288d29ab755b6499c114209381a4a
Jared Corker <jared.corker@gmail.com>
2023-12-20 11:31:52 +1100

    add opts to lazy setup to set colorscheme for lazy

--- .config/nvim/lua/jared/lazy-setup.lua
+++ .config/nvim/lua/jared/lazy-setup.lua
@@ -11,4 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("jared.plugins")
+local plugins = "jared.plugins"
+local opts = { install = { colorscheme = { "tokyonight" } } }
+
+require("lazy").setup(plugins, opts)
