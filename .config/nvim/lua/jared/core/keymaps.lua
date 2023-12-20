23b6c384776be57d585ca17852062be2e7681733
Jared Corker <jared.corker@gmail.com>
2023-12-20 12:01:06 +1100

    add save keymap

--- .config/nvim/lua/jared/core/keymaps.lua
+++ .config/nvim/lua/jared/core/keymaps.lua
@@ -9,6 +9,7 @@ keymap("n", "<C-h>", "<C-w>h")
 keymap("n", "<C-j>", "<C-w>j")
 keymap("n", "<C-k>", "<C-w>k")
 keymap("n", "<C-l>", "<C-w>l")
+keymap("n", "<leader>ss", ":w<CR>")
 
 -- Insert mode
 keymap("i", "kj", "<Esc>")
