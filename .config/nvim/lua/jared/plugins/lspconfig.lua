a9ce17977de0b986ed54a7fd2d49d0c5a74936ba
Jared Corker <jared.corker@gmail.com>
2023-12-28 21:12:03 +1100

    add go-to-definition and code-actions keymap and ignore global vim warning

--- .config/nvim/lua/jared/plugins/lspconfig.lua
+++ .config/nvim/lua/jared/plugins/lspconfig.lua
@@ -19,11 +19,21 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {'vim'},
+            },
+          },
+        },
+      })
       lspconfig.tsserver.setup({})
 
       local keymap = vim.keymap.set
       keymap('n', 'K', vim.lsp.buf.hover)
+      keymap('n', 'gd', vim.lsp.buf.definition)
+      keymap('n', '<leader>ca', vim.lsp.buf.code_action)
     end
   }
 }
