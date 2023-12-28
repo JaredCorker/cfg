bdcdfe48f4a9332fb9824e3c2f8af5f98a0b3d99
Jared Corker <jared.corker@gmail.com>
2023-12-28 19:41:33 +1100

    add single quit keymap and update lazy keymap

--- .config/nvim/lua/jared/core/keymaps.lua
+++ .config/nvim/lua/jared/core/keymaps.lua
@@ -4,7 +4,8 @@ vim.g.mapleader = " "
 
 -- Normal mode
 keymap("n", "<leader>qa", ":qa<CR>", { desc = "Quit all neovim" })
-keymap("n", "<leader>l", ":Lazy<CR>")
+keymap("n", "<leader>qq", ":q<CR>", { desc = "Quit neovim" })
+keymap("n", "<leader>ll", ":Lazy<CR>")
 keymap("n", "<C-h>", "<C-w>h")
 keymap("n", "<C-j>", "<C-w>j")
 keymap("n", "<C-k>", "<C-w>k")
