2a4558cf933f72a81f104f1d0188372b0e31b761
Jared Corker <jared.corker@gmail.com>
2023-12-28 22:30:20 +1100

    comment out harpoon 1-4 keymaps until alternative

--- .config/nvim/lua/jared/plugins/harpoon.lua
+++ .config/nvim/lua/jared/plugins/harpoon.lua
@@ -14,9 +14,9 @@ return {
     keymap("n", "<leader>h", function() harpoon:list():append() end, { desc = "Add file to harpoon" })
     keymap("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    keymap("n", "<C-u>", function() harpoon:list():select(1) end)
-    keymap("n", "<C-i>", function() harpoon:list():select(2) end)
-    keymap("n", "<C-o>", function() harpoon:list():select(3) end)
-    keymap("n", "<C-p>", function() harpoon:list():select(4) end)
+    -- keymap("n", "<C-u>", function() harpoon:list():select(1) end)
+    -- keymap("n", "<C-i>", function() harpoon:list():select(2) end)
+    -- keymap("n", "<C-o>", function() harpoon:list():select(3) end)
+    -- keymap("n", "<C-p>", function() harpoon:list():select(4) end)
   end
 }
