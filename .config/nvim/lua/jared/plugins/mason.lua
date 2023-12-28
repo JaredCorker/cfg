76e28c573d43dd1d2dcc29899df864215153e160
Jared Corker <jared.corker@gmail.com>
2023-12-28 20:20:16 +1100

    lazy load mason with keymap

--- .config/nvim/lua/jared/plugins/mason.lua
+++ .config/nvim/lua/jared/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-      vim.keymap.set("n", "<leader>m", "<cmd>Mason<CR>", { desc = "Open Mason" })
-    end
+  "williamboman/mason.nvim",
+  keys = {
+    { "<leader>m", "<cmd>Mason<CR>", desc = "Open Mason" }
+  },
+  config = true
 }
