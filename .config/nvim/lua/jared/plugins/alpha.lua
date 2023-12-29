f64df34ce980adb647b4ad53cb99983a6953143c
Jared Corker <jared.corker@gmail.com>
2023-12-29 22:28:09 +1100

    remove file tree from alpha

--- .config/nvim/lua/jared/plugins/alpha.lua
+++ .config/nvim/lua/jared/plugins/alpha.lua
@@ -25,7 +25,6 @@ return {
             dashboard.button('e', '  New file', ':ene <CR>'),
             dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
             dashboard.button('s', '  Find text', ':Telescope live_grep <CR>'),
-            dashboard.button('n', '  Open file tree', ':NvimTreeOpen <CR>'),
             dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
         }
 
