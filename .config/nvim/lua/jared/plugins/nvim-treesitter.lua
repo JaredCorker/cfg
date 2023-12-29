5c4539c53ca5e05d88d58f5c5570909ce1aca5b9
Jared Corker <jared.corker@gmail.com>
2023-12-29 22:46:13 +1100

    add back ts parsers recommended by docs

--- .config/nvim/lua/jared/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/jared/plugins/nvim-treesitter.lua
@@ -1,14 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function ()
+  config = function()
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "html" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "html" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
